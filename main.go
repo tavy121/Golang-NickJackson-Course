8a7cc219429ee98760e8249d631f41917cd2907e
Octavian Predusca <tavy177gmail.com>
2020-06-18 11:10:13 +0300

    Building Microservices with Go4

--- main.go
+++ main.go
@@ -13,12 +13,11 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+
+	productHandler := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	sm.Handle("/", productHandler)
 
 	s := &http.Server{
 		Addr:         ":9090",
