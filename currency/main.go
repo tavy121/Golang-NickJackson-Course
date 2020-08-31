142513b962601d1293254ecdcfba507694932f58
Octavian Predusca <tavy177gmail.com>
2020-08-31 14:56:05 +0300

    13 Introduction to gRPC and Protocol Buffers

---
+++ currency/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net"
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/tavy121/Go_Microservices/currency/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	protos "github.com/tavy121/Go_Microservices/currency/protos/currency"
+)
+
+func main() {
+	log := hclog.Default()
+
+	gs := grpc.NewServer()
+	cs := server.NewCurrency(log)
+
+	protos.RegisterCurrencyServer(gs, cs)
+
+	reflection.Register(gs)
+
+	l, err := net.Listen("tcp", ":9091")
+	if err != nil {
+		log.Error("Unable to listen", "error", err)
+		os.Exit(1)
+	}
+
+	gs.Serve(l)
+}
