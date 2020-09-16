7f46e70716f65de778b60f1793b35c0561dc12df
Octavian Predusca <tavy177gmail.com>
2020-09-16 18:42:04 +0300

    gRPC Bi-directional streaming part 2

--- currency/data/rates.go
+++ currency/data/rates.go
@@ -3,8 +3,10 @@ package data
 import (
 	"encoding/xml"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/hashicorp/go-hclog"
 )
@@ -34,6 +36,35 @@ func (e *ExchangeRates) GetRate(base, dest string) (float64, error) {
 	}
 
 	return dr / br, nil
+}
+
+func (e *ExchangeRates) MonitorRates(interval time.Duration) chan struct{} {
+	ret := make(chan struct{})
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case <-ticker.C:
+				for k, v := range e.rates {
+					change := (rand.Float64() / 10)
+					direction := rand.Intn(1)
+
+					if direction == 0 {
+						change = 1 - change
+					} else {
+						change = 1 + change
+					}
+
+					e.rates[k] = v * change
+				}
+
+				ret <- struct{}{}
+			}
+		}
+	}()
+
+	return ret
 }
 
 func (e *ExchangeRates) getRates() error {
